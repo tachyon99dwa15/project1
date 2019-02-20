3af5529dceddefafa69b430fadae774eddfb0b80
tachyon99dwa15 <47389889+tachyon99dwa15@users.noreply.github.com>
2019-02-19 20:02:24 -0800

    Add files via upload

---
+++ peter.php
@@ -0,0 +1,52 @@
+<?php
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Peter Chen</title>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+
+	<link href='css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+	<div class="container">
+
+		<h1>Peter Chen</h1>
+
+		<img src='peterchen.jpg' alt='Peter Chen'>
+
+		<h2>About Me</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+		</p>
+
+		<h2>Random Quote</h2>
+		<blockquote>
+			Do your own thing on your own terms and get what you came here for. -Oliver James		</blockquote>
+
+	</div>
+
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
+  $quotes[] = 'We must be willing to let go of the life we planned so as to have the life that is waiting for us.';
+  $quotes[] = 'Follow your bliss and the universe will open doors for you where there were only walls.';
+  $quotes[] = 'You must give up the life you planned in order to have the life that is waiting for you.';
+
+  srand ((double) microtime() * 400000);
+  $random_num = rand(0,count($quotes)-1);
+
+  echo ($quotes[$random_num]);
+  
+?>
