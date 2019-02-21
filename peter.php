17afd2579e038639c968888cc6672170c8928d06
Peter Chen <tachyon99@live.com>
2019-02-20 19:07:54 -0800

    Modified peter.php and deleted commented out css section

--- peter.php
+++ peter.php
@@ -10,10 +10,9 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
-<!	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
 
-<!	<link href='css/main.css' rel='stylesheet'>
+
+
 
 </head>
 <body>
@@ -44,7 +43,7 @@
 
 	</div>
 
-<!	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
 
 </body>
 </html>
